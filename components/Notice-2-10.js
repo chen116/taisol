1285d636f63b82c7a5fef080a38b0383e64d106d
Victor Chen <chen116@usc.edu>
2020-12-23 14:02:36 +0800

    a

--- components/Notice-2-10.js
+++ components/Notice-2-10.js
@@ -3,9 +3,9 @@ app.component('notice-2-10', {
     return {
 
       table1:[
-        {name: 'namee',self_stock:'ss',self_stock_per:"eee", fam_stock:"ff",fam_stock_per:"ee",other_stock:"oof",other_stock_pre:'dddd', name2:'ee2',relation:'eee'},
-        {name: 'namee',self_stock:'ss',self_stock_per:"eee", fam_stock:"ff",fam_stock_per:"ee",other_stock:"oof",other_stock_pre:'dddd', name2:'ee2',relation:'eee'},
-        {name: 'namee',self_stock:'ss',self_stock_per:"eee", fam_stock:"ff",fam_stock_per:"ee",other_stock:"oof",other_stock_pre:'dddd', name2:'ee2',relation:'eee'},
+        {name: 'namee',self_stock:'ss',self_stock_per:"eee", fam_stock:"ff",fam_stock_per:"ee",other_stock:"oof",other_stock_pre:'dddd', name2:'ee2',relation:'eee',note:'d'},
+        {name: 'namee',self_stock:'ss',self_stock_per:"eee", fam_stock:"ff",fam_stock_per:"ee",other_stock:"oof",other_stock_pre:'dddd', name2:'ee2',relation:'eee',note:'d'},
+        {name: 'namee',self_stock:'ss',self_stock_per:"eee", fam_stock:"ff",fam_stock_per:"ee",other_stock:"oof",other_stock_pre:'dddd', name2:'ee2',relation:'eee',note:'d'},
       ]
 
       
@@ -24,17 +24,17 @@ app.component('notice-2-10', {
 
   <h6><b>本公司股權比例占前十大之股東：   </b></h6>
 
-  <p>本公司股權比例占前十大之股東：   </p>
+  <p>109 年 4 月 7 日  單位：股</p>
   
   <table  class="table table-bordered table-hover">
     <thead>
     <tr>
-        <th rowspan="2">利害關係人</th>
-        <th colspan="2">關切的議題</th>
-        <th colspan="2">關切的議題</th>
-        <th colspan="2">關切的議題</th>
-        <th colspan="2">關切的議題</th>
-        <th rowspan="2">我們的努力</th>
+        <th rowspan="2">姓名（註一）</th>
+        <th colspan="2">本人持有股份</th>
+        <th colspan="2">配偶、未成年子女持有股份</th>
+        <th colspan="2">利用他人名義合計持有股份</th>
+        <th colspan="2">前十大股東互相間具有財務會計準則公報第六號關係人或為配偶、二等親以內之親屬關係者，其名稱或姓名及關係（註三）</th>
+        <th rowspan="2">備註</th>
     </tr>
     <tr>
       <th>ss1</th>
--- components/Notice-2-2.js
+++ components/Notice-2-2.js
@@ -33,7 +33,14 @@ app.component('notice-2-2', {
             {"name": "2-1title","self_stock":"2-1a", "self_stock_per":"ss" , "spouse_stock":"ss","spouse_stock_per":"qq",  "other_stock":"ssq","other_stock_per":"qaq","name2":"sq","relation":"pp","note":""},         
             {"name": "2-1title","self_stock":"2-1a", "self_stock_per":"ss" , "spouse_stock":"ss","spouse_stock_per":"qq",  "other_stock":"ssq","other_stock_per":"qaq","name2":"sq","relation":"pp","note":""},         
             {"name": "2-1title","self_stock":"2-1a", "self_stock_per":"ss" , "spouse_stock":"ss","spouse_stock_per":"qq",  "other_stock":"ssq","other_stock_per":"qaq","name2":"sq","relation":"pp","note":""},         
-          ]
+        ],
+
+      table4:[
+        {name: 'namee',self_stock:'ss',self_stock_per:"eee", fam_stock:"ff",fam_stock_per:"ee",other_stock:"oof",other_stock_pre:'dddd', name2:'ee2',relation:'eee',note:'d'},
+        {name: 'namee',self_stock:'ss',self_stock_per:"eee", fam_stock:"ff",fam_stock_per:"ee",other_stock:"oof",other_stock_pre:'dddd', name2:'ee2',relation:'eee',note:'d'},
+        {name: 'namee',self_stock:'ss',self_stock_per:"eee", fam_stock:"ff",fam_stock_per:"ee",other_stock:"oof",other_stock_pre:'dddd', name2:'ee2',relation:'eee',note:'d'},
+      ]
+
         
         }
 
@@ -140,13 +147,35 @@ app.component('notice-2-2', {
                 
 
 
-                <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" >
-                    <div class="accordion-body">
-                        <img class="rounded" src="./assets/images/img21.gif"/>
-                       
-                    
-                    </div>
-                </div>
+                <p>109 年 4 月 7 日  單位：股</p>
+                <table  class="table table-bordered table-hover">
+                  <thead>
+                  <tr>
+                      <th rowspan="2">姓名（註一）</th>
+                      <th colspan="2">本人持有股份</th>
+                      <th colspan="2">配偶、未成年子女持有股份</th>
+                      <th colspan="2">利用他人名義合計持有股份</th>
+                      <th colspan="2">前十大股東互相間具有財務會計準則公報第六號關係人或為配偶、二等親以內之親屬關係者，其名稱或姓名及關係（註三）</th>
+                      <th rowspan="2">備註</th>
+                  </tr>
+                  <tr>
+                    <th>ss1</th>
+                    <th>ss2</th>
+                    <th>ss3</th>
+                    <th>ss4</th>
+                    <th>ss5</th>
+                    <th>ss6</th>
+                    <th>ss7</th>
+                    <th>ss8</th>
+              
+                  </thead>
+                  <tbody>
+                  <tr v-for="row in table4">
+                    <td v-for="key in Object.keys(row)">{{ row[key] }}</td>
+                  </td>
+                  </tr> 
+                  </tbody>
+                </table>
             </div>
 
         </div>
